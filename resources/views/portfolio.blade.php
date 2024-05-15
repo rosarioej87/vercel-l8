7503dbab06871fd60b396b2583e1b4d92b3e6e40
rosarioej87 <57592877+rosarioej87@users.noreply.github.com>
2024-05-15 14:52:26 +0800

    Update portfolio.blade.php

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -79,7 +79,10 @@
     </div>
   <!-- End Portfolio Section -->
   </div>
-
+  <div>
+      <h1>{{ $data->title }}</h1>
+      <p>{{ $data->description }}</p>
+  </div>
   <!-- About Section -->
   <div class="w3-content w3-justify w3-text-grey w3-padding-32" id="about">
     <h2>About</h2>
